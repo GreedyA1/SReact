cd026380306ac2aeb4e4c134b5ebfb4da5504730
LifeAlchemist <m.vato122@gmail.com>
2018-10-14 20:37:06 +0400

    Ant Design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Button, version } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <Button type="primary" className="square" onClick={props.onClick}>
       {props.value}
-    </button>
+    </Button>
   );
 }
 
